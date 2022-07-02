b7196f536c0305b58806a7821a996c1ffc78d0ad
Yuya Furusawa <fullyou0798@gmail.com>
2022-07-02 21:03:24 +0900

    Add chapter4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,61 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+  @i
+  M=0
+
+(INLOOP)
+  @i
+  D=M
+  @8191
+  D=D-A
+  @LOOP
+  D;JGT
+
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+
+  @i
+  D=M
+  @SCREEN
+  A=A+D
+  M=-1
+
+  @i
+  M=M+1
+  @INLOOP
+  0;JMP
+
+(WHITE)
+  @i
+  D=M
+  @SCREEN
+  A=A+D
+  M=0
+
+  @i
+  M=M+1
+  @INLOOP
+  0;JMP
+
+
+// メモ
+//
+// while (True) {
+//   int i = 0;
+//   while (i <= 8191) {
+//     screen = SCREEN + i;
+//     if (KBD != 0) {
+//       Memory[screen] = -1;
+//     } else {
+//       Memory[screen] = 0;
+//     }
+//     i++;
+//   }
+// }
+
+// そもそも無限ループのコードなので最後に無限ループを明示する必要なし
